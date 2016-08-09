f1da621a736053f8af9e1396d86fc1776daac098
Manuel Widmer <manuel.widmer@swisscom.com>
2016-08-09 11:52:13 +0200

    update Gemfile and adapt test to assert with the merged patch for 100% pie graphs

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ source "http://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
-  gem "jeweler", "~> 1.6.4"
+  gem 'juwelier'
+  # development on windows 
+  gem 'win32console'
 end
 
+group :test do
+  gem 'minitest'
+  gem 'minitest-reporters'
+end
--- test/test_svg_graph.rb
+++ test/test_svg_graph.rb
@@ -1,6 +1,11 @@
-require 'minitest/autorun'
 require_relative '../lib/svggraph'
 require_relative '../lib/SVG/Graph/DataPoint'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = { :color => true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
 
 class TestSvgGraph < Minitest::Test
 
@@ -27,7 +32,7 @@ class TestSvgGraph < Minitest::Test
     fields = %w(Internet TV Newspaper Magazine Radio)
     #data1 = [2, 3, 1, 3, 1]
     #data2 = [0, 2, 1, 5, 4]
-    data1 = [0, 3, 0, 0, 1]
+    data1 = [0, 3, 0, 0, 0]
     data2 = [0, 6, 0, 0, 0]
     
     graph = SVG::Graph::Pie.new(
@@ -55,7 +60,7 @@ class TestSvgGraph < Minitest::Test
       fout.print( out )
     }
     assert_match(/TV 100%/, out, "100% text not found in graph") 
-    
+    assert_match(/circle/, out, "no circle was found in graph")
 
   end # test_pie_100_percent
   
