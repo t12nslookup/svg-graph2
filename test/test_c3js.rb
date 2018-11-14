530456a3850c16c26d7a71235f6ae35834640761
Manuel Widmer <m-widmer@gmx.ch>
2018-11-14 09:55:42 +0100

    add support for inlined javascript/css dependencies, add documentation and testcases

--- test/test_c3js.rb
+++ test/test_c3js.rb
@@ -14,7 +14,7 @@ class TestC3js < Minitest::Test
     @dataset2 = ['data2', 300, 20, 10, 40, 15, 25]
 
     #hash style
-    @graph_data = {
+    @hash_chart_spec = {
       bindto: "##{@bindto_id}",
       data: {
         columns: [
@@ -54,9 +54,9 @@ class TestC3js < Minitest::Test
         show: true
       }
     }
-    #puts JSON.pretty_generate(@graph_data)
-    @heredoc_chart =<<-HEREDOC
-    {
+    #puts JSON.pretty_generate(@hash_chart_spec)
+    @heredoc_chart_spec =<<-HEREDOC
+    var my_chart1 = c3.generate({
       // bindto is mandatory
       "bindto": "##{@bindto_id}",
       "data": {
@@ -95,7 +95,29 @@ class TestC3js < Minitest::Test
       "subchart": {
         "show": true
       }
-    }
+    });
+
+    setTimeout(function () {
+      my_chart1.load({
+          columns: [
+              ['data1', 230, 190, 300, 500, 300, 400]
+          ]
+      });
+    }, 1000);
+
+    setTimeout(function () {
+      my_chart1.load({
+            columns: [
+                ['data3', 130, 150, 200, 300, 200, 100]
+            ]
+        });
+    }, 1500);
+
+    setTimeout(function () {
+      my_chart1.unload({
+            ids: 'data1'
+        });
+    }, 2000);
     HEREDOC
 
 
@@ -111,17 +133,44 @@ class TestC3js < Minitest::Test
   end
 
   def test_burn
-    g = SVG::Graph::C3js.new("mychart1", @graph_data)
+    g = SVG::Graph::C3js.new()
+    g.add_chart_spec(@hash_chart_spec, "mychart1")
     write_file("hash_#{__method__}", g.burn)
 
-    g = SVG::Graph::C3js.new("mychart1", @heredoc_chart)
+    g = SVG::Graph::C3js.new()
+    g.add_chart_spec(@heredoc_chart_spec)
     write_file("heredoc_#{__method__}", g.burn)
   end # def test_burn
 
 
   def test_burn_svg_only
-    g = SVG::Graph::C3js.new("mychart1", @graph_data)
+    g = SVG::Graph::C3js.new()
+    g.add_chart_spec(@hash_chart_spec, "mychart1")
     write_file(__method__, g.burn_svg_only)
   end # def test_burn_svg_only
+
+  def test_invalid_file
+    opts = {
+      "inline_dependencies" => true,
+      "d3_js"  => "/path/to/local/copy/of/d3.min.js",
+      "c3_css" => "/path/to/local/copy/of/c3.min.css",
+      "c3_js"  => "/path/to/local/copy/of/c3.min.js"
+    }
+    assert_raises(RuntimeError) { |e|
+      g = SVG::Graph::C3js.new(opts)
+    }
+  end
+
+  def test_inlined_dependencies
+    opts = {
+      "inline_dependencies" => true,
+      "d3_js"  => File.expand_path("d3.min.js",__dir__),
+      "c3_css" => File.expand_path("c3.min.css",__dir__),
+      "c3_js"  => File.expand_path("c3.min.js",__dir__)
+    }
+    g = SVG::Graph::C3js.new(opts)
+    g.add_chart_spec(@heredoc_chart_spec)
+    write_file(__method__, g.burn)
+  end
 
 end # class TestC3js
